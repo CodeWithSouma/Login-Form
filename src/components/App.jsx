e961cf907ab9031eec76a3d7d526a64d69bcf38b
Souma <souma.hitech@gmail.com>
2020-08-19 23:40:24 +0530

    add a function that render depend on condition.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
+
+var isLogedIn = false;
+
+function renderConditionally(){
+  if (isLogedIn){
+    return <h1>Hello</h1>;
+  }else{
+    return (
       <form className="form">
         <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
         <button type="submit">Login</button>
       </form>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="container">
+      {renderConditionally()}
     </div>
+    
   );
 }
 
