0ed893a32bfad2220ba92d2ae85668f48975efaa
Souma <souma.hitech@gmail.com>
2020-08-20 00:47:16 +0530

    add register page.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import Login from "./Login";
-import Heading from "./Heading"
+import Register from "./Register"
+import Heading from "./Heading";
 
-var isLogedIn = false;
+var isLogedIn = true;
 
 function App() {
   return (
     <div className="container">
-      {isLogedIn?<Heading text= "Hello"/>:<Login/>}
+      {isLogedIn?
+            (
+              <div>
+              <Heading text = "Login"/>
+              <Login/>
+              </div>
+              ):(
+                <div>
+                <Heading text = "Register"/>
+                <Register/>
+                </div>
+              )}
     </div>
     
   );
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import Input from "./Input";
 import Button from "./Button";
 
-function Login() {
+
+function Register() {
     return(
         <form>
             <Input type="text" placeholder="Username"/>
             <Input type="password" placeholder="Password"/>
-            <Button name="Login"/>
+            <Input type="password" placeholder="Confirm Password"/>
+            <Button name="Register"/>
         </form>
     );
 }
 
-export default Login;
+export default Register;
